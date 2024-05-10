b6bb3b73c3985a70b9761966170cb41172dfd4ab
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-05-10 08:49:00 +0500

    qwe

--- lib/main.dart
+++ lib/main.dart
@@ -28,72 +28,78 @@ class _MainApp extends State<MainApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 11, 1, 44),
-      body: pages[currentpage],
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 11, 1, 44),
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    currentpage = 0;
-                  });
-                },
-                icon: Icon(
-                  Icons.home_rounded,
-                  size: 40,
-                  color: currentpage == 0 ? Colors.deepPurpleAccent : Colors.grey.shade400,
-                )),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    currentpage = 1;
-                  });
-                },
-                icon: Icon(
-                  Icons.search_rounded,
-                  size: 40,
-                  color: currentpage == 1 ? Colors.deepPurpleAccent : Colors.grey.shade400,
-                )),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    currentpage = 2;
-                  });
-                },
-                icon: Icon(
-                  CupertinoIcons.bookmark,
-                  size: 40,
-                  color: currentpage == 2 ? Colors.deepPurpleAccent : Colors.grey.shade400,
-                )),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    currentpage = 3;
-                  });
-                },
-                icon: Icon(
-                  CupertinoIcons.arrow_down_to_line,
-                  size: 40,
-                  color: currentpage == 3 ? Colors.deepPurpleAccent : Colors.grey.shade400,
-                )),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    currentpage = 4;
-                  });
-                },
-                icon: Icon(
-                  Icons.person,
-                  size: 40,
-                  color: currentpage == 4 ? Colors.deepPurpleAccent : Colors.grey.shade400,
-                ))
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          pages[currentpage],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.transparent, borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)), boxShadow: [BoxShadow(blurRadius: 200, spreadRadius: 10, color: Colors.grey.withOpacity(0.5))]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          currentpage = 0;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.home_rounded,
+                        size: 40,
+                        color: currentpage == 0 ? Colors.deepPurpleAccent : Colors.grey.shade400,
+                      )),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          currentpage = 1;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.search_rounded,
+                        size: 40,
+                        color: currentpage == 1 ? Colors.deepPurpleAccent : Colors.grey.shade400,
+                      )),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          currentpage = 2;
+                        });
+                      },
+                      icon: Icon(
+                        CupertinoIcons.bookmark,
+                        size: 40,
+                        color: currentpage == 2 ? Colors.deepPurpleAccent : Colors.grey.shade400,
+                      )),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          currentpage = 3;
+                        });
+                      },
+                      icon: Icon(
+                        CupertinoIcons.arrow_down_to_line,
+                        size: 40,
+                        color: currentpage == 3 ? Colors.deepPurpleAccent : Colors.grey.shade400,
+                      )),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          currentpage = 4;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.person,
+                        size: 40,
+                        color: currentpage == 4 ? Colors.deepPurpleAccent : Colors.grey.shade400,
+                      ))
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
